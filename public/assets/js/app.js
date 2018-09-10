65e0388120a1e1c438cc7ef45eb48cffe81f61ea
schmidgallm <schmidgallm@outlook.com>
2018-09-10 09:33:51 -0700

    static path to front end scripts and styles

---
+++ public/assets/js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(() => {
+
+    // focus on name on page load and hide success message
+    $('#name').focus();
+    $('#success-message').hide();
+    $('#email-message').hide();
+
+
+    // Event Listeners
+    $(document).on('click', '#submit', newMessage);
+
+    function newMessage(e) {
+
+        // Prevent form from submitting
+        e.preventDefault();
+
+        // Create new message objct 
+        const newMsg = {
+            name: $('#name').val().trim(),
+            email: $('#email').val().trim(),
+            message: $('#message').val().trim()
+        }
+
+        // Ajax Post Request
+        $.ajax('/submit', {
+            method: 'POST',
+            data: newMsg
+        }).then(() => {
+            swal({
+                    title: "Message Has Beent Sent!",
+                    text: "See You There!!",
+                    icon: "success",
+                    button: "Aww yiss!",
+                })
+                .then(() => {
+
+                    // clear input values
+                    $('#name').empty();
+                    $('#email').empty();
+                    $('#message').empty();
+
+                    //show success message
+                    $('#success-message').show().addClass('animated slideInLeft success-message-color');
+
+                    $('#name').focus();
+                });
+        });
+    }
+});
---
+++ public/assets/js/maps.js
@@ -0,0 +1,1 @@
+const myMap = require('../../../config/keys');
