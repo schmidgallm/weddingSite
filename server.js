84f2981e0f93dfe7ceae35fb1a1e19b55804ff8d
schmidgallm <schmidgallm@outlook.com>
2018-10-15 16:35:09 -0700

    if statemnt for node_env

--- server.js
+++ server.js
@@ -17,23 +17,27 @@ mongoose.connect(process.env.MONGODB_URI, () => {
     console.log('Now Connected to MongoDB');
 });
 
-// Set static content to public directory
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // // Init static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+    // Set static content to public directory
+    app.use(express.static(path.join(__dirname, 'public')));
 }
 
 // User morgan to log all requests
 app.use(logger('dev'));
 
 // Init hanlebars as view engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 //Body Parser Middleware 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 // Import routes from controller folder so server has access to them
