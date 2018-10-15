f4830c5263ef21d10f32546d4ab401575cbc5c9d
schmidgallm <schmidgallm@outlook.com>
2018-10-15 16:36:31 -0700

    reverse prior commit

--- server.js
+++ server.js
@@ -17,27 +17,18 @@ mongoose.connect(process.env.MONGODB_URI, () => {
     console.log('Now Connected to MongoDB');
 });
 
-
-
-// // Init static assets
-if (process.env.NODE_ENV === 'production') {
-    // Set static content to public directory
-    app.use(express.static(path.join(__dirname, 'public')));
-}
+// Set static content to public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // User morgan to log all requests
 app.use(logger('dev'));
 
 // Init hanlebars as view engine
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //Body Parser Middleware 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Import routes from controller folder so server has access to them
